5bdb25c07dda9395215535c3180ef54f284af92b
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-23 04:05:58 +0700

    add template 'render'

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,27 +1,59 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
 
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
-        // Описываем, что загрузится по адресу /
+
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
+
 
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
 
+
         app.get("users/{id}/post/{postId}", ctx -> {
             var userId = ctx.pathParamAsClass("id", Long.class).get();
             var postId = ctx.pathParamAsClass("postId", Long.class).get();
             ctx.result("User ID: " + userId + "\nPost Id: " + postId);
         });
+
+
+        app.get("/render", ctx -> ctx.render("index.jte"));
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParam("id");
+            var course = new Course("Java-разработчик", "Описание");
+            var page = new CoursePage(course);
+            ctx.render("show.jte", model("page", page));
+        });
+
+        app.get("/courses", ctx -> {
+            var courses = new ArrayList<>(List.of(
+                    new Course("Java-разработчик", "Описание"),
+                    new Course("PHP-разработчик", "Описание"),
+                    new Course("Python-разработчик", "Описание")));
+
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("index2.jte", model("page", page));
+        });
+
         app.start(7070);
     }
 }
