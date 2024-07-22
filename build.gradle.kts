c89a87982573b1d5f94b3f3c582168f6dca9161b
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-23 04:04:28 +0700

    add rendering

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    id("java")
     application
+    id("io.freefair.lombok") version "8.4"
 }
 
 application {
@@ -18,10 +18,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.javalin:javalin:6.1.4")
-    implementation("org.slf4j:slf4j-simple:2.0.13")
-    implementation("io.javalin:javalin-rendering:6.1.6")
     implementation("gg.jte:jte:3.1.12")
+    implementation("io.javalin:javalin:6.2.0")
+    implementation("io.javalin:javalin-rendering:6.1.6")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
