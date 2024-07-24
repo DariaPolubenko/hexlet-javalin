8d866d3cd574f0f5ab23a1fe6a7174ac1c1aae08
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-25 04:41:24 +0700

    update CoursesPage.java

--- src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
@@ -1,14 +1,23 @@
 package org.example.hexlet.dto.courses;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.example.hexlet.Course;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Getter
 public class CoursesPage {
     private List<Course> courses;
-    private String header;
+
+    @Setter
+    public String header;
+
+    @Setter
+    public String term;
+
+    public CoursesPage(List<Course> courses) {
+        this.courses = courses;
+    }
+
 }
