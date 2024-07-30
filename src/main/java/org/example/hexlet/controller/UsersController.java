220f92afaf16fe6ee895faf611128fccf2432a97
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-31 00:07:37 +0700

    create UsersController.java

---
+++ src/main/java/org/example/hexlet/controller/UsersController.java
@@ -0,0 +1,55 @@
+package org.example.hexlet.controller;
+
+import io.javalin.http.Context;
+import io.javalin.validation.ValidationException;
+import org.apache.commons.text.StringEscapeUtils;
+import org.example.hexlet.dto.users.BuildUserPage;
+import org.example.hexlet.dto.users.UsersPage;
+import org.example.hexlet.model.User;
+import org.example.hexlet.repository.UserRepository;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+import static org.apache.commons.lang3.StringUtils.capitalize;
+
+public class UsersController {
+    public static void showUsers(Context ctx) {
+        var users = UserRepository.getEntities();
+        var page = new UsersPage(users);
+        ctx.render("users/showUsers.jte", model("page", page));
+    }
+
+    public static void build(Context ctx) {
+        var page = new BuildUserPage();
+        ctx.render("users/build.jte", model("page", page));
+    }
+
+    public static void getPost(Context ctx) {
+        var userId = ctx.pathParamAsClass("id", Long.class).get();
+        var postId = ctx.pathParamAsClass("postId", Long.class).get();
+        ctx.result("User ID: " + userId + "\nPost Id: " + postId);
+    }
+
+    public static void testAttack(Context ctx) {
+        var id = ctx.pathParam("id");
+        var escapedId = StringEscapeUtils.escapeHtml4(id);
+        ctx.contentType("text/html");
+        ctx.result(escapedId);
+    }
+
+    public static void create(Context ctx) {
+        var name = capitalize(ctx.formParam("name"));
+        var email = ctx.formParam("email").trim().toLowerCase();
+        try {
+            var passwordConfirmation = ctx.formParam("passwordConfirmation");
+            var password = ctx.formParamAsClass("password", String.class)
+                    .check(value -> value.equals(passwordConfirmation), "Пароли не совпадают")
+                    .get();
+            var user = new User(name, email, password);
+            UserRepository.save(user);
+            ctx.redirect("/users");
+        } catch (ValidationException e) {
+            var page = new BuildUserPage(name, email, e.getErrors());
+            ctx.render("users/build.jte", model("page", page));
+        }
+    }
+}
