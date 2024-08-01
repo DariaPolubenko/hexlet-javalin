57e849811bccb0a39fea1fbd16edc57fca47f63a
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-02 00:59:54 +0700

    add parent class

--- src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
@@ -2,12 +2,13 @@ package org.example.hexlet.dto.courses;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.example.hexlet.dto.BasePage;
 import org.example.hexlet.model.Course;
 
 import java.util.List;
 
 @Getter
-public class CoursesPage {
+public class CoursesPage extends BasePage {
     private List<Course> courses;
     @Setter
     public String header;
