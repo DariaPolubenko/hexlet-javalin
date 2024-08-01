5f8ce06c684cf40f18086fe0ffe317ae44f8a984
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-01 20:35:31 +0700

    add method 'destroy' in SessionsController.java

--- src/main/java/org/example/hexlet/controller/SessionsController.java
+++ src/main/java/org/example/hexlet/controller/SessionsController.java
@@ -2,12 +2,13 @@ package org.example.hexlet.controller;
 
 import io.javalin.http.Context;
 
+import static org.example.hexlet.NamedRoutes.mainPath;
+
 public class SessionsController {
 
     public static void build(Context ctx) {
         ctx.render("sessions/build.jte");
     }
-
 
     public static void create(Context ctx) {
         var nickname = ctx.formParam("nickname");
@@ -16,13 +17,11 @@ public class SessionsController {
         // Тут должна быть проверка пароля
 
         ctx.sessionAttribute("currentUser", nickname);
-        ctx.redirect("/");
+        ctx.redirect(mainPath());
     }
 
-    /*
     public static void destroy(Context ctx) {
         ctx.sessionAttribute("currentUser", null);
         ctx.redirect("/");
     }
-     */
 }
