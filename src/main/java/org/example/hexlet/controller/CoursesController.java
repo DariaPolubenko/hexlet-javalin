6731ee724436242475685e91fb4673a691e8c4c3
Daria Polubenko <daria.polubenko@gmail.com>
2024-08-02 01:00:16 +0700

    update CoursesController.java

--- src/main/java/org/example/hexlet/controller/CoursesController.java
+++ src/main/java/org/example/hexlet/controller/CoursesController.java
@@ -3,6 +3,7 @@ package org.example.hexlet.controller;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import io.javalin.validation.ValidationException;
+import org.example.hexlet.NamedRoutes;
 import org.example.hexlet.dto.courses.BuildCoursePage;
 import org.example.hexlet.dto.courses.CoursePage;
 import org.example.hexlet.dto.courses.CoursesPage;
@@ -29,15 +30,19 @@ public class CoursesController {
             ctx.render("courses/showCourses.jte", model("page", page));
 
         } else {
+            String flash = ctx.consumeSessionAttribute("flash");
             var header = "Курсы по программированию";
             var page = new CoursesPage(CourseRepository.getEntities());
             page.setHeader(header);
+            page.setFlash(flash);
             ctx.render("courses/showCourses.jte", model("page", page));
         }
     }
 
     public static void build(Context ctx) {
+        String flash = ctx.consumeSessionAttribute("flash2");
         var page = new BuildCoursePage();
+        page.setFlash(flash);
         ctx.render("courses/build.jte", model("page", page));
     }
 
@@ -50,21 +55,23 @@ public class CoursesController {
     }
 
     public static void create(Context ctx) {
-        String name = "";
-        String description = "";
-
         try {
-            name = ctx.formParamAsClass("name", String.class)
+            var name = ctx.formParamAsClass("name", String.class)
                     .check(value -> value.length() > 2, "Имя не может содержать менее 3 символов")
                     .get();
-            description = ctx.formParamAsClass("description", String.class)
+            var description = ctx.formParamAsClass("description", String.class)
                     .check(value -> value.length() > 10, "Описание не может содержать менее 10 символов")
                     .get();
             var course = new Course(name, description);
             CourseRepository.save(course);
-            ctx.redirect("/courses");
+            ctx.sessionAttribute("flash", "Курс успешно добавлен!");
+            ctx.redirect(NamedRoutes.coursesPath());
 
         } catch (ValidationException e) {
+            var name = ctx.formParam("name");
+            var description = ctx.formParam("description");
+
+            ctx.sessionAttribute("flash2", "Курс не был добавлен!");
             var page = new BuildCoursePage(name, description, e.getErrors());
             ctx.render("courses/build.jte", model("page", page));
         }
